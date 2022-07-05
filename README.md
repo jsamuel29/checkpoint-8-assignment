372ee4960eb3cbcbc0222839e71023a765e63494
Jonathan Samuel <jonathansamuel83@gmail.com>
2022-07-05 13:44:24 -0500

    assignment 2

---
+++ README.md
@@ -0,0 +1,4 @@
+# checkpoint 8 assignment
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/jsamuel1/pen/MWQZJzQ](https://codepen.io/jsamuel1/pen/MWQZJzQ).
+
---
+++ dist/script.js
@@ -0,0 +1,27 @@
+var attendees = [
+  'Jacob',
+  'Matthew',
+  'Harold',
+  'James',
+  'Josh'
+];
+
+
+//For loop assignment
+for (var i = 0; i < attendees.length; i++) {
+ console.log(attendees[i]);
+}
+
+// while loop assignment
+var i = 0;
+while (i < attendees.length) {
+ console.log(attendees[i]);
+ i++;
+}
+
+//do while loop assignment
+var i = 0;
+do {
+  console.log(attendees[i]);
+  i++;
+} while (i < attendees.length)
